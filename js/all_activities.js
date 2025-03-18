cbdcdc252ad2144ef213f82d57b70afbb7660dcb
Nafiz43 <imtiznafiz@gmail.com>
2025-03-17 23:58:18 -0700

    automated activities section

---
+++ js/all_activities.js
@@ -0,0 +1,85 @@
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const roles = [
+      {
+        title: "Volunteer Co-Chair",
+        link: "https://conf.researchr.org/committee/ase-2024/ase-2024-student-volunteers-program-committee",
+        venue: "ASE-2024",
+        venueLink: "https://conf.researchr.org/home/ase-2024"
+      },
+      {
+        title: "Session Chair: SE for AI",
+        link: "https://conf.researchr.org/track/ase-2024/ase-2024-research?",
+        venue: "ASE-2024",
+        venueLink: "https://conf.researchr.org/home/ase-2024"
+      },
+      {
+        title: "Session Chair: Smart Contract & BlockChain",
+        link: "https://conf.researchr.org/track/ase-2024/ase-2024-research?",
+        venue: "ASE-2024",
+        venueLink: "https://conf.researchr.org/home/ase-2024"
+      },
+      {
+        title: "Reviewer",
+        venue: "SAGE Open",
+        venueLink: "https://chi2023.acm.org/"
+      },
+      {
+        title: "Reviewer",
+        venue: "CHI 2023",
+        venueLink: "https://drive.google.com/file/d/1N1PFF3CgG6IWv6YXdNbLijXegnZINwo9/view?usp=sharing"
+      },
+      {
+        title: "Program Committee Member and Reviewer",
+        venue: "IEEE World Conference on Applied Intelligence and Computing (AIC) 2022",
+        venueLink: "https://aic2022.scrs.in/"
+      },
+      {
+        title: "Chief Technical Officer",
+        venue: "BEPZA Recruitment Exam - 2022",
+        venueLink: "https://www.bepza.gov.bd/"
+      },
+      {
+        title: "Chief Technical Officer",
+        venue: "AFMC Admission Test - 2021",
+        venueLink: "https://afmc.edu.bd/"
+      },
+      {
+        title: "Event Coordinator",
+        venue: "Mobile App Contest, MIST Inter-University ICT Innovation Fest 2021"
+      },
+      {
+        title: "Technical Member",
+        venue: "MIST Inter-University ICT Innovation Fest 2021"
+      }
+    ];
+
+    const listContainer = document.getElementById("activity-list");
+
+    roles.forEach(role => {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = `
+        <p>
+          <i class="fa fa-tasks" aria-hidden="true"></i>
+          ${role.link ? `<a href="${role.link}" target="_blank">${role.title}</a>,` : `${role.title},`}
+          ${role.venueLink ? `<a href="${role.venueLink}" target="_blank">${role.venue}</a>` : role.venue}
+        </p>
+      `;
+      listContainer.appendChild(listItem);
+    });
+  });
+
+
+
+//   <!-- <li><p><i class="fa fa-tasks" aria-hidden="true"></i>
+//   Participated in Intra MIST Programming and Gaming Competition 2018</a></p></li>
+ 
+//  <li><p><i class="fa fa-tasks" aria-hidden="true"></i>
+//   Participated in MIST CSE Fest Programming Contest 2018.</a></p></li>
+
+  
+
+
+//   <li><p><i class="fa fa-tasks" aria-hidden="true"></i>
+//   Participated in MIST Inter-University Programming Contest (IUPC) 2019.</a></p></li> -->
+//   <!-- https://conf.researchr.org/home/ase-2024 -->
