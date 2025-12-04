67da9f5539cd981d0e8b2cc2290cfd79be5a623b
Nafiz Imtiaz Khan <43680716+Nafiz43@users.noreply.github.com>
2025-12-03 22:03:05 -0800

    Update awards.js

--- js/awards.js
+++ js/awards.js
@@ -10,8 +10,9 @@
         refLink: "https://mist.ac.bd/department/cse/announcement/193/team_uvc_purge_conquered_international_medical_robotics_challege"
       },
       {
-        title: "<a href=\"https://innovate.ucdavis.edu/people/nafiz-imtiaz-khan" target=\"_blank\">Leaders for the Future</a> Fellowship, Mike and Renée Child Institute for Innovation and Entrepreneurship, 2025-2026"
+        title: "<a href=\"https://innovate.ucdavis.edu/people/nafiz-imtiaz-khan\" target=\"_blank\">Leaders for the Future</a> Fellowship, Mike and Renée Child Institute for Innovation and Entrepreneurship, 2025-2026"
       },
+
       {
         title: "<a href=\"https://grad.ucdavis.edu/travel-awards\" target=\"_blank\">Graduate Student Travel Award</a>, 2025"
       },
